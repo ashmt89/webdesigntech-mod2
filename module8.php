9403f2cffd4a0554f6c484f305679b88f319ef17
ashmt89 <ashmt89@gmail.com>
2024-03-15 16:14:26 -0400

    formatted module 8 php form submission details

--- module8.php
+++ module8.php
@@ -1,75 +1,15 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <title>Ashley's Website - Module 8</title>
     <meta charset="utf-8">
-    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/main.css">
-
-    <script>
-        function validateForm() {
-            var fName = document.forms["myForm"]["fname"].value;
-            var lName = document.forms["myForm"]["lname"].value;
-            var phone = document.forms["myForm"]["phone"].value;
-            var email = document.forms["myForm"]["email"].value;
-            var msg = document.forms["myForm"]["message"].value;
-
-            if (fName == "") {
-                alert("Please enter your first name");
-                return false;
-            }
-            if (lName == "") {
-                alert("Please enter your last name");
-                return false;
-            }
-            if (phone == "") {
-                alert("Please enter your phone number");
-                return false;
-            }
-            if (!validatePhone(phone)) {
-                alert("Please enter a valid phone number (i.e. 123-456-7890)");
-                return false;
-            }
-            if (email == "") {
-                alert("Please enter your email address");
-                return false;
-            }
-            if (!validateEmail(email)) {
-                alert("Please enter a valid email address (i.e. youremail@website.com)");
-                return false;
-            }
-            if (msg == "") {
-                alert("Please enter a message");
-                return false;
-            }
-
-            alert("Thank you, your form has been submitted!");
-            return true;
-        }
-
-        function validatePhone(phone) {
-            // Regular expression to validate phone number format   
-            var re = /^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/;
-
-            // Check if the phone number matches the regular expression
-            return re.test(phone);
-        }
-
-        function validateEmail(email) {
-            // Regular expression to validate email address format
-            var re = /\S+@\S+\.\S+/;
-
-            // Check if the email address matches the regular expression
-            return re.test(email);
-        }
-
-    </script>
-
 </head>
 
 <body>
+
     <!-- Wrapper -->
     <div id="wrapper">
 
@@ -88,32 +28,37 @@
             </div>
         </nav>
 
-        <!-- Header -->
-        <header id="header">
+            <!-- Header -->
+            <header id="header">
+                <center>
+                    <h1>Module 8</h1>
+                </center>
+            </header>
 
-            <center>
-                <h1>Module 8</h1>
-            </center>
-        </header>
+            <!-- Main -->
+            <section id="main">
 
-        <!-- Main -->
-        <section id="main">
+                <center>
+                    <h2>Thank you, <?php echo $_POST["fname"]; ?>, your submission was successful!</h2>
+                    <br><br>
+                    <b>Submitted form details:</b>
+                    <br>
+                    <b>First Name:</b> <?php echo $_POST["fname"]; ?>
+                    <br>
+                    <b>Last Name:</b> <?php echo $_POST["lname"]; ?>
+                    <br>
+                    <b>Phone:</b> <?php echo $_POST["phone"]; ?>
+                    <br>
+                    <b>Email:</b> <?php echo $_POST["email"]; ?>
+                    <br>
+                    <b>Subject:</b> <?php echo $_POST["subject"]; ?>
+                    <br>
+                    <b>Message:</b> <?php echo $_POST["message"]; ?>
+                    <br>
+                    <b>Preferred Contact Method:</b> <?php echo $_POST["pref_contact"]; ?>
+                </center>
 
-            <center>
-                Thank you, <?php echo $_POST["fname"]; ?>, your submission was successful!
-                <br>
-                Submitted form details:
-                <br>
-                First Name: <?php echo $_POST["fname"]; ?>
-                Last Name: <?php echo $_POST["lname"]; ?>
-                Phone: <?php echo $_POST["phone"]; ?>
-                Email: <?php echo $_POST["email"]; ?>
-                Subject: <?php echo $_POST["subject"]; ?>
-                Message: <?php echo $_POST["message"]; ?>
-                Preferred Contact Method: <?php echo $_POST["pref_contact"]; ?>
-            </center>
-
-        </section>
+            </section>
 
     </div>
 
