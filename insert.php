a4063d683d1815aaed8a073691f08683eac123ea
ashmt89 <ashmt89@gmail.com>
2024-03-22 21:29:19 -0400

    working on php page

--- insert.php
+++ insert.php
@@ -30,28 +30,13 @@
         </nav>
 
             <?php
-            $servername = "localhost";
-            $username = "username";
-            $password = "password";
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password);
-
-            // Check connection
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-            echo "Connected successfully";
-            ?>
-
-            <?php
             $servername = "sql301.infinityfree.com";
             $username = "if0_35788056";
             $password = "R0scoestinks";
             $dbname = "if0_35788056_module10";
 
             // Create connection
-            $conn = new mysqli($sql301.infinityfree.com, $if0_35788056, $R0scoestinks, $if0_35788056_module10);
+            $conn = new mysqli($servername, $username, $passwords, $dbname);
 
             // Check connection
             if ($conn->connect_error) {
